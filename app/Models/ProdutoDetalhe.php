17bd91cf8b0e1ad4d88fd0e49c2a1ba477ebb89f
Thiago Braga <thiagobraga2a@gmail.com>
2025-11-05 07:50:44 -0300

    feat: adiciona belongsTo para exibir dados do produto

--- app/Models/ProdutoDetalhe.php
+++ app/Models/ProdutoDetalhe.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class ProdutoDetalhe extends Model
 {
     protected $fillable = ['produto_id', 'comprimento', 'altura', 'largura', 'unidade_id'];
+
+    public function produto(){
+        return $this->belongsTo('App\Models\Produto');
+        // belongsTo -> Pertence "a"
+        // produto_detalhes pertence a produtos;
+
+    }   
 }
+
