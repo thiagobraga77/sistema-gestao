af95d7e7c72a235a677986aebeb8c5c66b94d27c
Thiago Braga <thiagobraga2a@gmail.com>
2025-11-08 10:50:05 -0300

    chore: aprimora o relacionamento prático belongsToMany no Eloquent ORM

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -21,4 +21,12 @@ class Item extends Model
     // 1 registro relacionado em produto_detalhes (fk) -> produto_id
     // produtos (pk) -> id
     // hasOne -> é o lado pai que tem um único registro relacionado
+    public function pedidos() {
+        return $this->belongsToMany('App\Models\Pedido', 'pedidos_produtos', 'produto_id', 'pedido_id');
+
+        /*
+        3 - Representa o nome da FK da tabela mapeada pelo model na tabela de relacionamento
+        4 - Representa o nome da FK da tabela mapeada pelo model no relacionameto que estamos implementando
+        */
+    }
 }
