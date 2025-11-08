064e37d1a2380b41915844adb1310ef3c4a88a5e
Thiago Braga <thiagobraga2a@gmail.com>
2025-11-08 10:13:48 -0300

    feat: implementa relacionamento belongsToMany entre produtos e pedidos

--- app/Http/Controllers/PedidoProdutoController.php
+++ app/Http/Controllers/PedidoProdutoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PedidoProduto;
 use Illuminate\Http\Request;
 use App\Models\Pedido;
 use App\Models\Produto;
@@ -22,6 +23,7 @@ class PedidoProdutoController extends Controller
     public function create(Pedido $pedido)
     {
         $produtos = Produto::all();
+        // $pedido->produtos; // eager loading
         return view('app.pedido_produto.create', ['pedido' => $pedido, 'produtos' => $produtos]);
     }
 
@@ -30,13 +32,24 @@ class PedidoProdutoController extends Controller
      */
     public function store(Request $request, Pedido $pedido)
     {
-        echo '<pre>';
-        print_r($pedido);
-        echo '<pre>';
-        echo '<hr>';
-        echo '<pre>';
-        print_r($request->all());
-        echo '<pre>';
+        $regras = [
+            'produto_id' => 'exists:produtos,id'
+        ];
+        $feedback = [
+            'produto_id.exists' => 'O produto informado não existe'
+        ];
+
+        $request->validate($regras, $feedback);
+        echo $pedido->id. ' - '.$request->get('produto_id');
+
+        $pedidoProduto = new PedidoProduto();
+        $pedidoProduto->pedido_id = $pedido->id;
+        $pedidoProduto->produto_id = $request->get('produto_id');
+        $pedidoProduto->save();
+
+        return redirect()->route('pedido-produto.create', ['pedido' => $pedido->id]);
+
+        
     }
 
     /**
